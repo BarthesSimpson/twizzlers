19caef1f6fdfe2871315e20d1f04ec4c622f0817
BarthesSimpson <d.s.frenk@gmail.com>
2018-09-22 22:29:21 -0700

    add tests and operators

--- example.js
+++ example.js
@@ -4,7 +4,9 @@ const { sum } = require('./src/operators')
 const twizzlified = Twizzlify(sum, null, sum)
 
 async function main() {
-  console.log(await twizzlified([1, 2, 3, 4, 5, 6, 7, 8]))
+  // console.log(await twizzlified([1, 2, 3, 4, 5, 6, 7, 8]))
+  console.log(await twizzlified(Array.from({ length: 10 }, (_, k) => k + 1)))
+
   process.exit(0)
 }
 main()
